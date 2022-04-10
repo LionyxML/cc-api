e2134b781f7d5399838faff2823d6be0fcb1e6b6
LionyxML <rahul.juliato@gmail.com>
2022-04-10 10:06:46 -0300

    feat: adds index.js and scripts start / dev

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+// App INIT
+const app = express();
+
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  return res.send("<h3>API server is running</h3>");
+});
+
+// ENV Configs
+const PORT = process.env.PORT || 5050;
+
+// APP Listen
+app.listen(PORT, () => {
+  console.log(`CC API server started on port: ${PORT}`);
+});
