485d8e9177354edb46cd1159d8c43c959da1bd0e
LionyxML <rahul.juliato@gmail.com>
2022-04-10 11:12:14 -0300

    feat: adds art to listening start

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,25 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-// App INIT
+const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
+app.disable("x-powered-by");
 
 // Test route
 app.get("/", (_req, res) => {
   return res.send("<h3>API server is running</h3>");
 });
 
-// ENV Configs
-const PORT = process.env.PORT || 5050;
-
-// APP Listen
 app.listen(PORT, () => {
+  const welcomeArt = `
+  ____ ____              _    ____ ___
+ / ___/ ___|            / \\  |  _ \\_ _|
+| |  | |      _____    / _ \\ | |_) | |
+| |__| |___  |_____|  / ___ \\|  __/| |
+ \\____\\____|         /_/   \\_\\_|  |___|
+  `;
+  console.log(welcomeArt);
   console.log(`CC API server started on port: ${PORT}`);
 });
