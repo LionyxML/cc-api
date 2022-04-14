1573a1d645b579c3003ccac0478cc7b6dd86ea46
LionyxML <rahul.juliato@gmail.com>
2022-04-14 00:16:40 -0300

    feat: swagger

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,6 +1,9 @@
 const swaggerAutogen = require("swagger-autogen")();
 
 const outputFile = "./src/swagger/swagger_output.json";
-const endpointsFiles = ["./src/api/routes/root.ts"];
+const endpointsFiles = [
+  "./src/api/routes/root.ts",
+  "./src/api/routes/users.ts",
+];
 
 swaggerAutogen(outputFile, endpointsFiles);
