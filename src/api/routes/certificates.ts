83a176d03c09a269639509f3b50573ccfff03af6
LionyxML <rahul.juliato@gmail.com>
2022-04-20 22:09:40 -0300

    feat: route for retrieving certificates

--- src/api/routes/certificates.ts
+++ src/api/routes/certificates.ts
@@ -70,4 +70,46 @@ export default (app: Router) => {
         );
     }
   );
+
+  app.get(
+    "/certificates/list",
+    passport.authenticate("jwt", {
+      session: false,
+    }),
+    async (req, res) => {
+      /*  TODO: fix it here
+          #swagger.tags = ['Certificates']
+          #swagger.summary = "Get logged in user's certificates"
+          #swagger.parameters['Authorization'] = {
+            in: 'header',
+            description: 'A JWT bearer',
+            required: true,
+          }
+          #swagger.responses[200] = {
+            description: 'Certificates listed.',
+          }
+      */
+
+      try {
+        const userId = jwt.decode(
+          req.headers.authorization?.split(" ")[1] || ""
+        ) as userIdType;
+
+        const certificates = await Certificate.findAll({
+          where: { UserId: userId?.id },
+          raw: true,
+        });
+
+        return res.status(200).json({
+          status: "success",
+          certificates: certificates,
+        });
+      } catch {
+        return res.status(400).json({
+          status: "error",
+          msg: "Error on getting certificates.",
+        });
+      }
+    } // async
+  );
 };
