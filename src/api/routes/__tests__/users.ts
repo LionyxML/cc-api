2638d16dea2089f4de4888c1fcddc85bc797f31d
LionyxML <rahul.juliato@gmail.com>
2022-04-24 15:17:01 -0300

    feat: tests for the user registration endpoint

---
+++ src/api/routes/__tests__/users.ts
@@ -0,0 +1,159 @@
+import request from "supertest";
+import express from "express";
+import appLoader from "../../../loaders";
+import config from "../../../config";
+import { faker } from "@faker-js/faker";
+
+const app = express();
+const apiPrefix = config.api.prefix;
+
+const imageAllowed =
+  "data:image/png;base64," +
+  Buffer.from(Array(Number(config.maxProfileSize)))
+    .fill("R")
+    .toString("base64");
+
+const imageBTAllowed =
+  "data:image/png;base64," +
+  Buffer.from(Array(Number(config.maxProfileSize) + 1))
+    .fill("R")
+    .toString("base64");
+
+beforeAll(async () => {
+  await appLoader({ expressApp: app });
+});
+
+describe("POST /users", () => {
+  it("must have all fields filled in", function (done) {
+    request(app)
+      .post(`${apiPrefix}/users/register`)
+      .send({})
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(
+        400,
+        {
+          status: "error",
+          msg: "All fields must be filled in.",
+        },
+        done
+      );
+  });
+
+  it("should return 400 & valid error response to profile pic bigger then allowed", function (done) {
+    request(app)
+      .post(`${apiPrefix}/users/register`)
+      .send({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        userName: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: "123123123",
+        passwordConfirmation: "123123123",
+        profilePic: imageBTAllowed,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400, /Profile Pic should not be bigger then/, done);
+  });
+
+  it("should return 400 if passwords do not match", function (done) {
+    request(app)
+      .post(`${apiPrefix}/users/register`)
+      .send({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        userName: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        passwordConfirmation: "123123123",
+        profilePic: imageAllowed,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400, /Passwords do not match/, done);
+  });
+
+  it("should return 200 for a valid user entry", function (done) {
+    request(app)
+      .post(`${apiPrefix}/users/register`)
+      .send({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        userName: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: "123123123",
+        passwordConfirmation: "123123123",
+        profilePic: imageAllowed,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201, /User is now registered/, done);
+  });
+
+  it("should return 400 if userName is already taken", function (done) {
+    request(app)
+      .post(`${apiPrefix}/users/register`)
+      .send({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        userName: "fooUser",
+        email: faker.internet.email(),
+        password: "123123123",
+        passwordConfirmation: "123123123",
+        profilePic: imageAllowed,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201, /User is now registered/)
+      .end(function () {
+        request(app)
+          .post(`${apiPrefix}/users/register`)
+          .send({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            userName: "fooUser",
+            email: faker.internet.email(),
+            password: "123123123",
+            passwordConfirmation: "123123123",
+            profilePic: imageAllowed,
+          })
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(400, /Username is already taken/, done);
+      });
+  });
+
+  it("should return 400 if email is already registered", function (done) {
+    request(app)
+      .post(`${apiPrefix}/users/register`)
+      .send({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        userName: faker.internet.userName(),
+        email: "foo@bar.net",
+        password: "123123123",
+        passwordConfirmation: "123123123",
+        profilePic: imageAllowed,
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201, /User is now registered/)
+      .end(function () {
+        request(app)
+          .post(`${apiPrefix}/users/register`)
+          .send({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            userName: `${faker.internet.userName()}--another`,
+            email: "foo@bar.net",
+            password: "123123123",
+            passwordConfirmation: "123123123",
+            profilePic: imageAllowed,
+          })
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(400, /E-mail already registered/, done);
+      });
+  });
+});
